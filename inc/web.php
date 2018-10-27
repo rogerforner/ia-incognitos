ced108d3daff77f89aad385519fb20b58a09a27b
Roger Forner Fabre <sir.rogerforner@gmail.com>
2018-10-27 22:27:44 +0200

    Meta Tags OK
    Es posible se borre Schema por JSON-LD

--- inc/web.php
+++ inc/web.php
@@ -24,8 +24,8 @@
 // Por defecto se inserta la versión.
 // =============================================================================
 function webQuitarMetaVersion() {
-    $nombreSitio = get_bloginfo('name');
-	return '<meta name="generator" content="'. $nombreSitio .'" />';
+    $miTema = wp_get_theme();
+	return '<meta name="generator" content="'. $miTema->get("Name") .' v'. $miTema->get("Version") .', by Roger Forner Fabre" />';
 }
 add_filter('the_generator', 'webQuitarMetaVersion');
 
@@ -44,12 +44,93 @@ add_filter('style_loader_src', 'webQuitarQueryString', 15, 1);
 | ------------------------------------------------------------------------------
 | The Open Graph protocol
 | ------------------------------------------------------------------------------
-| - Schema.org
-| - Open Graph Meta Tags
+| - Meta Tags & Open Graph
+| - Schema JSON-LD
 |
 */
 
-// Schema.org
+// Meta Tags & Open Graph
+// =============================================================================
+function openGraph() {
+	global $post;
+	
+	$codigoIdioma   = get_bloginfo('language');
+	$nombreSitioWeb = get_bloginfo('name');
+	$autor          = get_author_name($post->post_author);
+
+	// Obtener el Título, la descripción y la URL canónica.
+	// =========================================================================
+	if (is_home() || is_front_page() || is_archive() || is_category() ||
+		is_tag() || is_tax() || is_search() || is_404() || is_date()) {
+        $titulo      = $nombreSitioWeb;
+        $descripcion = get_bloginfo('description');
+		$urlCanonica = get_bloginfo('url');
+
+    } else {
+		$titulo = get_the_title();
+
+		if (has_excerpt($post->ID)) {
+			$descripcion = get_the_excerpt();
+		} else {
+			$descripcion = get_bloginfo('description');
+		}
+
+		$urlCanonica = get_permalink();
+	}
+
+	// Obtener la imagen.
+	// =========================================================================
+	$thumbnail_src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
+	$imagen        = $thumbnail_src[0];
+
+	// Obtener la taxonmía que represente las etiquetas.
+	// =========================================================================
+	if (is_singular("post")) {
+		$etiquetas = get_the_terms($post->ID, "post_tag");
+	} elseif (is_singular("project")) {
+		$etiquetas = get_the_terms($post->ID, "project_tag");
+	}
+
+
+	
+	// MOSTRAR DATOS
+	// =========================================================================
+	echo '
+	<meta name="web_author" content="Roger Forner Fabre">
+	<meta name="copyright" content="'.$nombreSitioWeb.'">
+	<meta name="author" content="'.$autor.'">
+	<meta name="description" content="'.$descripcion.'">
+	';
+	$insertarComa = 1;
+	if ($etiquetas) {
+		echo '<meta name="keywords" content="';
+		foreach($etiquetas as $tag) {
+			echo $tag->name;
+			echo ($insertarComa < count($etiquetas)) ? "," : "";
+			$insertarComa++;
+		}
+		echo '">';
+	}
+
+	// Facebook + Twitter
+	echo '
+	<meta property="og:locale" content="'.$codigoIdioma.'"/>
+	<meta property="og:site_name" content="'.$nombreSitioWeb.'"/>
+	<meta property="og:title" content="'.$titulo.'"/>
+	<meta property="og:url" content="'.$urlCanonica.'"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:description" content="'.$descripcion.'"/>
+	<meta property="og:image" content="'.$imagen.'" />
+	<meta name="twitter:title" content="'.$titulo.'"/>
+	<meta name="twitter:url" content="'.$urlCanonica.'"/>
+	<meta name="twitter:description" content="'.$descripcion.'"/>
+	<meta name="twitter:image:src" content="'.$imagen.'">
+	<meta name="twitter:card" content="summary_large_image"/>
+	';
+}
+add_action('wp_head', 'openGraph', 5);
+
+// Schema JSON-LD
 // =============================================================================
 function openGraphHtmlOut() {
 	if( is_single() ) {
@@ -76,61 +157,3 @@ function openGraphHtmlOut() {
     return 'lang="'.$codigoIdioma.'" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://ogp.me/ns/fb#" itemscope itemtype="http://schema.org/'.$cpt.'"';
 }
 add_filter('language_attributes', 'openGraphHtmlOut');
-
-// Open Graph Meta Tags
-// =============================================================================
-function openGraph() {
-	global $post;
-
-	$codigoIdioma   = get_bloginfo('language');
-	$nombreSitioWeb = get_bloginfo('name');
-
-	// Título, descripción y URL canónica.
-	// =========================================================================
-	if (is_home() || is_front_page() || is_404() || is_archive() || is_category() || is_tag() || is_tax() || is_date()) {
-        $titulo      = get_bloginfo('name');
-        $descripcion = get_bloginfo('description');
-		$urlCanonica = get_bloginfo('url');
-
-    } else {
-		$titulo = get_the_title();
-
-		if (has_excerpt($post->ID)) {
-			$descripcion = get_the_excerpt();
-		} else {
-			$descripcion = get_bloginfo('description');
-		}
-
-		$urlCanonica = get_permalink();
-	}
-
-	// Imagen.
-	// =========================================================================
-	$thumbnail_src = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full');
-	$imagen        = $thumbnail_src[0];
-	
-	// Meta Etiquetas Open Graph.
-	// =========================================================================
-	//MOSTRAR
-	echo '
-	<!-- Facebook Open Graph -->
-	<meta property="og:locale" content="'.$codigoIdioma.'"/>
-	<meta property="og:site_name" content="'.$nombreSitioWeb.'"/>
-	<meta property="og:title" content="'.$titulo.'"/>
-	<meta property="og:url" content="'.$urlCanonica.'"/>
-	<meta property="og:type" content="website"/>
-	<meta property="og:description" content="'.$descripcion.'"/>
-	<meta property="og:image" content="'.$imagen.'" />
-	<!-- Google+ / Schema.org -->
-	<meta itemprop="name" content="'.$titulo.'"/>
-	<meta itemprop="description" content="'.$descripcion.'"/>
-	<meta itemprop="image" content="'.$imagen.'">
-	<!-- Twitter Cards -->
-	<meta name="twitter:title" content="'.$titulo.'"/>
-	<meta name="twitter:url" content="'.$urlCanonica.'"/>
-	<meta name="twitter:description" content="'.$descripcion.'"/>
-	<meta name="twitter:image:src" content="'.$imagen.'">
-	<meta name="twitter:card" content="summary_large_image"/>
-	';
-}
-add_action('wp_head', 'openGraph', 5);
