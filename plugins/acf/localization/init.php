ebdb999a925ee25f64737e9a67ef0aa2f10a679a
Roger Forner Fabre <sir.rogerforner@gmail.com>
2018-12-12 19:33:29 +0100

    Compatibilidad con Polylang OK

--- plugins/acf/localization/init.php
+++ plugins/acf/localization/init.php
@@ -1,3 +1,16 @@
 <?php
 
-require_once get_stylesheet_directory() . '/plugins/acf/localization/polylang.php';
+/*
+| ------------------------------------------------------------------------------
+| Localización por plugins
+| ------------------------------------------------------------------------------
+| Código utilizado para permitir la traducción a través de plugins.
+| - Polylang
+|
+*/
+
+// Polylang
+// =============================================================================
+if (function_exists( 'pll_current_language' )) {
+    require_once get_stylesheet_directory() . '/plugins/acf/localization/polylang.php';
+}
--- plugins/acf/localization/polylang.php
+++ plugins/acf/localization/polylang.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+| ------------------------------------------------------------------------------
+| Idiomas
+| ------------------------------------------------------------------------------
+| Definir los idiomas para permitir las traducciones de ACF.
+| https://www.advancedcustomfields.com/resources/multilingual-custom-fields/
+|
+*/
+
 function my_acf_settings_default_language() {
     return pll_default_language();
 }
